e7f1333033c04c40b2e8ceb639adad3774fbc432
zhuqiqi <zhuqiqi@skyguard.com.cn>
2022-12-14 17:56:43 +0800

    feat: 温习context

--- channel/context/withTimeout/test.go
+++ channel/context/withTimeout/test.go
@@ -18,7 +18,7 @@ LOOP:
 		time.Sleep(time.Millisecond * 10) // 假设正常连接数据库耗时10毫秒
 		select {
 		case <-ctx.Done(): // 50毫秒后自动调用
-			fmt.Println(ctx.Err())
+			fmt.Println(ctx.Err()) // context deadline exceeded
 			break LOOP
 		default:
 		}
--- channel/context/withValue/test.go
+++ channel/context/withValue/test.go
@@ -36,12 +36,13 @@ LOOP:
 func main() {
 	// 设置一个50毫秒的超时
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*50)
+	defer cancel()
 	// 在系统的入口中设置trace code传递给后续启动的goroutine实现日志数据聚合
 	ctx = context.WithValue(ctx, TraceCode("TRACE_CODE"), "12512312234")
 	wg.Add(1)
 	go worker(ctx)
 	time.Sleep(time.Second * 5)
-	cancel() // 通知子goroutine结束
+	//cancel() // 通知子goroutine结束
 	wg.Wait()
 	fmt.Println("over")
 }

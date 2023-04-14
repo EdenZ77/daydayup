11b5d07527aacef963e0e24292f093f57b2149fc
zhuqiqi <zhuqiqi@skyguard.com.cn>
2023-04-14 16:50:27 +0800

    feat: unsafe加深

--- base/slice/01/main.go
+++ base/slice/01/main.go
@@ -1,17 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+)
 
 func main() {
+	//arr := []int32{8, 8, 1, 1, 1, 1, 3, 3, 4, 4, 5}
+	//removeDuplic := RemoveDuplic(arr)
+	//fmt.Println(removeDuplic)
 
-	s := []int{1, 1, 1}
-	f(s)
-	fmt.Println(s)
+	jsonStruct()
 }
 
-func f(s []int) {
-	for i := range s {
-		s[i]++
+func RemoveDuplic(slice []int32) []int32 {
+	var resultList []int32
+	tempMap := map[int32]byte{}
+	for _, item := range slice {
+		tempLen := len(tempMap)
+		tempMap[item] = 0
+		if len(tempMap) != tempLen {
+			resultList = append(resultList, item)
+		}
 	}
-	s = append(s, 7)
+	return resultList
+}
+
+func jsonInter() {
+	msg := map[string]interface{}{
+		"cat":       3,
+		"sourceCat": []string{"22", "33"},
+		"source":    "sophos-add",
+		"uuid":      "uuid==",
+		"url":       "urlStr==",
+		"url_type":  "sophosType==",
+	}
+	msgJsonStr, err := jsoniter.MarshalToString(msg)
+	if err != nil {
+		return
+	}
+	fmt.Println(msgJsonStr)
+}
+
+type RabbitmqMsg struct {
+	Cat       int32    `json:"cat"`
+	SourceCat []string `json:"sourceCat"`
+	Source    string   `json:"source"`
+	Uuid      string   `json:"uuid"`
+	Url       string   `json:"url"`
+	UrlType   string   `json:"url_type"`
+}
+
+func jsonStruct() {
+	msg := RabbitmqMsg{
+		Cat:       3,
+		SourceCat: []string{"22", "33"},
+		Source:    "sophos-add",
+		Uuid:      "uuid==",
+		Url:       "urlStr==",
+		UrlType:   "sophosType==",
+	}
+	msgJsonStr, err := jsoniter.MarshalToString(msg)
+	if err != nil {
+		return
+	}
+	fmt.Println(msgJsonStr)
 }

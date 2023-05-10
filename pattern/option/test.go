6e3da0eed6e8de04d9d23a91b573bcfc3ee8e56e
zhuqiqi <zhuqiqi@skyguard.com.cn>
2023-05-10 18:36:32 +0800

    feat: options模式

--- pattern/option/test.go
+++ pattern/option/test.go
@@ -2,57 +2,57 @@ package main
 
 import "fmt"
 
-// Option
+// TestOption
 /*
 我们可能需要为Option的字段指定默认值
 Option的字段成员可能会发生变更
 
 可阅读grpc源码中dialOptions使用的option模式, 它引入了接口，和示例稍有不同
 */
-type Option struct {
+type TestOption struct {
 	A string
 	B string
 	C int
 }
 
-func newOption(a, b string, c int) *Option {
-	return &Option{
+func newTestOption(a, b string, c int) *TestOption {
+	return &TestOption{
 		A: a,
 		B: b,
 		C: c,
 	}
 }
 
-type OptionFunc func(*Option)
+type OptionFunc func(*TestOption)
 
 // WithA 利用闭包为每个字段编写一个设置值的With函数
 func WithA(a string) OptionFunc {
-	return func(o *Option) {
+	return func(o *TestOption) {
 		o.A = a
 	}
 }
 
 func WithB(b string) OptionFunc {
-	return func(o *Option) {
+	return func(o *TestOption) {
 		o.B = b
 	}
 }
 
 func WithC(c int) OptionFunc {
-	return func(o *Option) {
+	return func(o *TestOption) {
 		o.C = c
 	}
 }
 
 var (
-	defaultOption = &Option{
+	defaultOption = &TestOption{
 		A: "A",
 		B: "B",
 		C: 100,
 	}
 )
 
-func newOption2(opts ...OptionFunc) (opt *Option) {
+func newOption2(opts ...OptionFunc) (opt *TestOption) {
 	opt = defaultOption
 	for _, o := range opts {
 		o(opt)
@@ -61,11 +61,14 @@ func newOption2(opts ...OptionFunc) (opt *Option) {
 }
 
 func main() {
-	x := newOption("nazha", "小王子", 10)
-	fmt.Println(x)
-	x = newOption2()
-	fmt.Println(x)
-	x = newOption2(
+	//x := newTestOption("nazha", "小王子", 10)
+	//fmt.Println(x)
+	//x = newOption2()
+	//fmt.Println(x)
+
+	// 思想就是当不传入任何参数时，会有默认值，当需要修改某个字段时，传入相应的WithXxx(value)
+	// 将修改行为封装成了函数，这可能是go的设计哲学吧。
+	x := newOption2(
 		WithA("沙河娜扎"),
 		WithC(250),
 	)

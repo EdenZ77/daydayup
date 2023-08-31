cf840e04535a9e60f3719aa0eb07b27c08c47df1
zhuqiqi <zhuqiqi@skyguard.com.cn>
2023-08-31 16:58:48 +0800

    feat: 并发

--- concurrent/waitgroup/test1/test1.go
+++ concurrent/waitgroup/test1/test1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"sync"
 	"time"
 )
@@ -46,4 +47,42 @@ func main() {
 	wg.Wait()
 	// 输出当前计数器的值
 	fmt.Println(counter.Count())
+
+	var testWg WaitGroupX
+	dumpMethodSet(&testWg)
+}
+
+func xx(l sync.Locker) {
+
+}
+
+func dumpMethodSet(i interface{}) {
+	dynTyp := reflect.TypeOf(i)
+
+	if dynTyp == nil {
+		fmt.Printf("there is no dynamic type\n")
+		return
+	}
+
+	n := dynTyp.NumMethod()
+	if n == 0 {
+		fmt.Printf("%s's method set is empty!\n", dynTyp)
+		return
+	}
+
+	fmt.Printf("%s's method set:\n", dynTyp)
+	for j := 0; j < n; j++ {
+		fmt.Println("-", dynTyp.Method(j).Name)
+	}
+	fmt.Printf("\n")
+}
+
+type noCopyX struct{}
+
+// Lock is a no-op used by -copylocks checker from `go vet`.
+func (*noCopyX) Lock()   {}
+func (*noCopyX) Unlock() {}
+
+type WaitGroupX struct {
+	noCopyX
 }

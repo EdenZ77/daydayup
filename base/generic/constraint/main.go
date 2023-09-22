18224e2404bbd73644de05b307a4a330d58683f7
zhuqiqi <zhuqiqi@skyguard.com.cn>
2023-09-22 16:54:55 +0800

    泛型

--- base/generic/constraint/main.go
+++ base/generic/constraint/main.go
@@ -5,11 +5,11 @@ import "fmt"
 // stringify_without_zero.go
 
 func StringifyWithoutZero[T fmt.Stringer](s []T) (ret []string) {
-	var zero T
+	//var zero T
 	for _, v := range s {
-		if v == zero { // 编译器报错：invalid operation: v == zero (incomparable types in type set)
-			continue
-		}
+		//if v == zero { // 编译器报错：invalid operation: v == zero (incomparable types in type set)
+		//	continue
+		//}
 		ret = append(ret, v.String())
 	}
 	return ret

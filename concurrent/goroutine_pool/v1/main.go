bbff6d113d312866354efeb2b69ed67f84ee35c3
eden <2660996862@qq.com>
2023-10-06 16:34:09 +0800

    增加readme.md

--- concurrent/goroutine_pool/v1/main.go
+++ concurrent/goroutine_pool/v1/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bytedance/gopkg/util/gopool"
 )
 
+// 参考资料：https://juejin.cn/post/7086443265309818894#heading-1
 func main() {
 	gopool.Go(func() {
 		/// do your job
---
+++ readme.md
@@ -0,0 +1,9 @@
+## 小框架分析
+- [x] 字节协程池gopool
+- [ ] singflight
+
+
+
+## 源码分析
+- [ ] sync.Mutex
+- [ ] sync.Once

f3a5d0bb19092f7f67778db0577db243ac6c9bc5
zhuqiqi <zhuqiqi@skyguard.com.cn>
2023-10-07 11:10:47 +0800

    reflect.DeepEqual基本使用

--- base/reflect/deep-equal/deep_equal.go
+++ base/reflect/deep-equal/deep_equal.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	m1 := map[int]interface{}{1: []int{1, 2, 3}, 2: 3, 3: "a"}
+	m2 := map[int]interface{}{1: []int{1, 2, 3}, 2: 3, 3: "a"}
+	if reflect.DeepEqual(m1, m2) {
+		fmt.Println("相等")
+	}
 }

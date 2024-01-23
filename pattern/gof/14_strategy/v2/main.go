0c616b7ac6916e01548d4371542a026b8f29791d
zhuqiqi <zhuqiqi@skyguard.com.cn>
2024-01-23 18:14:32 +0800

    策略设计模式

--- pattern/gof/14_strategy/v2/main.go
+++ pattern/gof/14_strategy/v2/main.go
@@ -1,73 +1,62 @@
 package main
 
+import "fmt"
+
 /*
-参考资料：GPT
+参考资料：https://www.yuque.com/aceld/lfhu8y/maozfp
 
-策略模式（Strategy Pattern）是一种行为设计模式，它定义了一系列算法，并将每个算法封装起来，使它们可以互换使用。
-策略模式让算法的变化独立于使用算法的客户端。
+商场促销有策略A（0.8折）策略B（消费满200，返现100），用策略模式模拟场景
 */
-import (
-	"fmt"
-	"sort"
-)
 
-// SortStrategy 排序策略接口
-type SortStrategy interface {
-	Sort([]int)
+// SellStrategy 销售策略
+type SellStrategy interface {
+	// GetPrice 根据原价得到售卖价
+	GetPrice(price float64) float64
 }
 
-// BubbleSortStrategy 冒泡排序策略
-type BubbleSortStrategy struct{}
+type StrategyA struct{}
 
-// Sort 实现冒泡排序算法
-func (b BubbleSortStrategy) Sort(data []int) {
-	fmt.Println("Sorting using bubble sort")
-	for i := 0; i < len(data)-1; i++ {
-		for j := 0; j < len(data)-1-i; j++ {
-			if data[j] > data[j+1] {
-				data[j], data[j+1] = data[j+1], data[j]
-			}
-		}
+func (sa *StrategyA) GetPrice(price float64) float64 {
+	fmt.Println("执行策略A, 所有商品打八折")
+	return price * 0.8
+}
+
+type StrategyB struct{}
+
+func (sb *StrategyB) GetPrice(price float64) float64 {
+	fmt.Println("执行策略B, 所有商品满200 减100")
+
+	if price >= 200 {
+		price -= 100
 	}
+
+	return price
 }
 
-// QuickSortStrategy 快速排序策略
-type QuickSortStrategy struct{}
-
-// Sort 实现快速排序算法
-func (q QuickSortStrategy) Sort(data []int) {
-	fmt.Println("Sorting using quick sort")
-	sort.Ints(data) // 这里为了简化，直接使用了sort包的Ints方法
+// Goods 环境类
+type Goods struct {
+	Price    float64
+	Strategy SellStrategy
 }
 
-// Context 上下文
-type Context struct {
-	strategy SortStrategy
+func (g *Goods) SetStrategy(s SellStrategy) {
+	g.Strategy = s
 }
 
-// SetStrategy 设置排序策略
-func (c *Context) SetStrategy(strategy SortStrategy) {
-	c.strategy = strategy
-}
-
-// Sort 调用策略方法进行排序
-func (c *Context) Sort(data []int) {
-	c.strategy.Sort(data)
+func (g *Goods) SellPrice() float64 {
+	fmt.Println("原价值 ", g.Price, " .")
+	return g.Strategy.GetPrice(g.Price)
 }
 
 func main() {
-	data := []int{9, 5, 3, 7, 1}
-	context := Context{}
+	nike := Goods{
+		Price: 200.0,
+	}
+	//上午 ，商场执行策略A
+	nike.SetStrategy(new(StrategyA))
+	fmt.Println("上午nike鞋卖", nike.SellPrice())
 
-	// 策略模式允许客户端在运行时选择算法或者行为，从而使算法的变更和使用算法的客户端解耦。
-
-	// 使用冒泡排序策略
-	context.SetStrategy(BubbleSortStrategy{})
-	context.Sort(data)
-	fmt.Println(data)
-
-	// 使用快速排序策略
-	context.SetStrategy(QuickSortStrategy{})
-	context.Sort(data)
-	fmt.Println(data)
+	//下午， 商场执行策略B
+	nike.SetStrategy(new(StrategyB))
+	fmt.Println("下午nike鞋卖", nike.SellPrice())
 }

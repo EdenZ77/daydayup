3179e30ea25ba1183dea42ac1a5c46d82c46863b
eden <2660996862@qq.com>
2024-03-26 21:34:04 +0800

    快排非递归

--- algo/zuo/ti_xi_study/class05/Code03_QuickSortRecursiveAndUnrecursive/Code03_QuickSortRecursiveAndUnrecursive.go
+++ algo/zuo/ti_xi_study/class05/Code03_QuickSortRecursiveAndUnrecursive/Code03_QuickSortRecursiveAndUnrecursive.go
@@ -6,8 +6,8 @@ import (
 	"time"
 )
 
-// 荷兰国旗问题
-func netherlandsFlag(arr []int, L int, R int) (int, int) {
+// netherlandsFlag 解决荷兰国旗问题，返回等于区域的左右下标
+func netherlandsFlag(arr []int, L, R int) (int, int) {
 	if L > R {
 		return -1, -1
 	}
@@ -18,95 +18,148 @@ func netherlandsFlag(arr []int, L int, R int) (int, int) {
 	more := R
 	index := L
 	for index < more {
-		if arr[index] < arr[R] {
+		if arr[index] == arr[R] {
+			index++
+		} else if arr[index] < arr[R] {
+			swap(arr, index, less+1)
+			index++
 			less++
-			arr[index], arr[less] = arr[less], arr[index]
-			index++
-		} else if arr[index] > arr[R] {
-			more--
-			arr[index], arr[more] = arr[more], arr[index]
 		} else {
-			index++
+			swap(arr, index, more-1)
+			more--
 		}
 	}
-	arr[more], arr[R] = arr[R], arr[more]
+	swap(arr, more, R)
 	return less + 1, more
 }
 
-// 快排递归版本
-func quickSort1(arr []int, L int, R int) {
+// swap 交换数组中两个元素的位置
+func swap(arr []int, i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+
+// quickSort1 快排递归版本
+func quickSort1(arr []int) {
+	if len(arr) < 2 {
+		return
+	}
+	process(arr, 0, len(arr)-1)
+}
+
+// process 快排的处理过程
+func process(arr []int, L, R int) {
 	if L >= R {
 		return
 	}
-	rand.Seed(time.Now().UnixNano())
-	pivot := L + rand.Intn(R-L+1)
-	arr[pivot], arr[R] = arr[R], arr[pivot]
-	el, er := netherlandsFlag(arr, L, R)
-	quickSort1(arr, L, el-1)
-	quickSort1(arr, er+1, R)
+	swap(arr, L+rand.Intn(R-L+1), R)
+	equalL, equalR := netherlandsFlag(arr, L, R)
+	process(arr, L, equalL-1)
+	process(arr, equalR+1, R)
 }
 
-type op struct {
-	l int
-	r int
+// Op 快排非递归版本需要的辅助类
+type Op struct {
+	l, r int
 }
 
-// 快排非递归版本，使用栈
+// quickSort2 快排非递归版本，使用栈来执行
 func quickSort2(arr []int) {
 	if len(arr) < 2 {
 		return
 	}
-	stack := make([]op, 0)
-	stack = append(stack, op{0, len(arr) - 1})
+	N := len(arr)
+	swap(arr, rand.Intn(N), N-1)
+	equalL, equalR := netherlandsFlag(arr, 0, N-1)
+	// 用栈这个结构来记录需要处理的区间，用户自己定义栈来代替递归的系统栈
+	stack := []Op{{0, equalL - 1}, {equalR + 1, N - 1}}
 	for len(stack) > 0 {
-		cur := stack[len(stack)-1]
-		stack = stack[:len(stack)-1] // Pop
-		if cur.l < cur.r {
-			rand.Seed(time.Now().UnixNano())
-			pivot := cur.l + rand.Intn(cur.r-cur.l+1)
-			arr[pivot], arr[cur.r] = arr[cur.r], arr[pivot]
-			el, er := netherlandsFlag(arr, cur.l, cur.r)
-			stack = append(stack, op{cur.l, el - 1})
-			stack = append(stack, op{er + 1, cur.r})
+		current := stack[len(stack)-1]
+		// 弹出栈顶元素，也就是切片的最后一个元素
+		stack = stack[:len(stack)-1]
+		// 如果当前区间的左边界小于右边界，说明当前区间还没有处理完
+		if current.l < current.r {
+			swap(arr, current.l+rand.Intn(current.r-current.l+1), current.r)
+			// 使用荷兰国旗确定当前区间的等于区域
+			equalL, equalR = netherlandsFlag(arr, current.l, current.r)
+			// 将当前区间的左右两部分分别压入栈中, 顺序无所谓; 等待后面弹出栈顶元素时处理
+			stack = append(stack, Op{current.l, equalL - 1}, Op{equalR + 1, current.r})
 		}
+		// 如果当前区间的左边界大于等于右边界，说明当前区间已经处理完，继续弹出栈顶元素，直至栈为空
 	}
 }
 
-// 快排非递归版本，使用队列
+// quickSort3 快排非递归版本，使用队列来执行
 func quickSort3(arr []int) {
 	if len(arr) < 2 {
 		return
 	}
-	queue := make([]op, 0)
-	queue = append(queue, op{0, len(arr) - 1})
+	N := len(arr)
+	swap(arr, rand.Intn(N), N-1)
+	equalL, equalR := netherlandsFlag(arr, 0, N-1)
+	queue := []Op{{0, equalL - 1}, {equalR + 1, N - 1}}
 	for len(queue) > 0 {
-		cur := queue[0]
-		queue = queue[1:] // Dequeue
-		if cur.l < cur.r {
-			rand.Seed(time.Now().UnixNano())
-			pivot := cur.l + rand.Intn(cur.r-cur.l+1)
-			arr[pivot], arr[cur.r] = arr[cur.r], arr[pivot]
-			el, er := netherlandsFlag(arr, cur.l, cur.r)
-			queue = append(queue, op{cur.l, el - 1})
-			queue = append(queue, op{er + 1, cur.r})
+		current := queue[0]
+		// 弹出队列头部元素，也就是切片的第一个元素
+		queue = queue[1:]
+		if current.l < current.r {
+			swap(arr, current.l+rand.Intn(current.r-current.l+1), current.r)
+			// 使用荷兰国旗确定当前区间的等于区域
+			equalL, equalR = netherlandsFlag(arr, current.l, current.r)
+			// 将当前区间的左右两部分分别压入队列中, 顺序无所谓; 等待后面弹出队列头部元素时处理
+			queue = append(queue, Op{current.l, equalL - 1}, Op{equalR + 1, current.r})
 		}
+		// 如果当前区间的左边界大于等于右边界，说明当前区间已经处理完，继续弹出队列头部元素，直至队列为空
 	}
 }
 
-// 测试
+// 可以发现，对于非递归版本的快排，使用栈和队列都可以，只是弹出元素的顺序不同，栈是后进先出，队列是先进先出，说明顺序无所谓，主要是记录需要处理的区间即可
+
+// generateRandomArray 生成随机数组（用于测试）
+func generateRandomArray(maxSize, maxValue int) []int {
+	arr := make([]int, rand.Intn(maxSize+1))
+	for i := range arr {
+		arr[i] = rand.Intn(maxValue+1) - rand.Intn(maxValue)
+	}
+	return arr
+}
+
+// isEqual 对比两个数组（用于测试）
+func isEqual(arr1, arr2 []int) bool {
+	if (arr1 == nil && arr2 != nil) || (arr1 != nil && arr2 == nil) {
+		return false
+	}
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	for i := range arr1 {
+		if arr1[i] != arr2[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	arr1 := []int{3, 1, 5, 7, 2, 4, 6, 8}
-	arr2 := make([]int, len(arr1))
-	copy(arr2, arr1)
-	arr3 := make([]int, len(arr1))
-	copy(arr3, arr1)
-
-	quickSort1(arr1, 0, len(arr1)-1)
-	fmt.Println("Sorted with quickSort1:", arr1)
-
-	quickSort2(arr2)
-	fmt.Println("Sorted with quickSort2:", arr2)
-
-	quickSort3(arr3)
-	fmt.Println("Sorted with quickSort3:", arr3)
+	rand.Seed(time.Now().UnixNano())
+	testTime := 500000
+	maxSize := 100
+	maxValue := 100
+	succeed := true
+	fmt.Println("test begin")
+	for i := 0; i < testTime; i++ {
+		arr1 := generateRandomArray(maxSize, maxValue)
+		arr2 := make([]int, len(arr1))
+		arr3 := make([]int, len(arr1))
+		copy(arr2, arr1)
+		copy(arr3, arr1)
+		quickSort1(arr1)
+		quickSort2(arr2)
+		quickSort3(arr3)
+		if !isEqual(arr1, arr2) || !isEqual(arr1, arr3) {
+			succeed = false
+			break
+		}
+	}
+	fmt.Println("test end")
+	fmt.Println("测试", testTime, "组是否全部通过：", succeed)
 }

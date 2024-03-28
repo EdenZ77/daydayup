5cab64cd3feabbe20a5d4432b35983ff427c2b88
eden <2660996862@qq.com>
2024-03-29 07:48:13 +0800

    堆

--- algo/zuo/ti_xi_study/class06/Code02_Heap/Code02_Heap.go
+++ algo/zuo/ti_xi_study/class06/Code02_Heap/Code02_Heap.go
@@ -1,5 +1,123 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+type MyMaxHeap struct {
+	heap []int
+	// 堆的大小
+	limit int
+	// 堆中有多少个数
+	size int
+}
+
+func NewMyMaxHeap(limit int) *MyMaxHeap {
+	return &MyMaxHeap{
+		heap:  make([]int, limit),
+		limit: limit,
+		size:  0,
+	}
+}
+
+// IsEmpty 返回堆是否为空
+func (h *MyMaxHeap) IsEmpty() bool {
+	return h.size == 0
+}
+
+// IsFull 返回堆是否已满
+func (h *MyMaxHeap) IsFull() bool {
+	return h.size == h.limit
+}
+
+// Push 把值 value 加入到大根堆中
+func (h *MyMaxHeap) Push(value int) error {
+	if h.IsFull() {
+		return errors.New("heap is full")
+	}
+	h.heap[h.size] = value
+	h.heapInsert(h.size)
+	h.size++
+	return nil
+}
+
+// Pop 用户此时，让你返回最大值，并且在大根堆中，把最大值删掉
+// 剩下的数，依然保持大根堆组织
+func (h *MyMaxHeap) Pop() (int, error) {
+	if h.IsEmpty() {
+		return 0, errors.New("heap is empty")
+	}
+	ans := h.heap[0]
+	h.size--
+	h.heap[0] = h.heap[h.size]
+	h.heapify(0)
+	return ans, nil
+}
+
+// 新加进来的数，现在停在了index位置，请依次往上移动，
+// 移动到0位置，或者干不掉自己的父亲了，停！
+func (h *MyMaxHeap) heapInsert(index int) {
+	// 这个判断处理了两种情况：
+	// 1. index == 0，h.heap[0] !> h.heap[(0-1)/2] = h.heap[0]，移动到0位置，停！
+	// 2. 或者干不掉自己的父亲了，停！
+	for h.heap[index] > h.heap[(index-1)/2] {
+		// 交换index和index的父节点的值
+		h.swap(index, (index-1)/2)
+		// index更新为index的父节点
+		index = (index - 1) / 2
+	}
+}
+
+// 从index位置，往下看，不断的下沉
+// 停：较大的孩子都不再比index位置的数大，或者已经没孩子了
+func (h *MyMaxHeap) heapify(index int) {
+	// 左孩子的下标
+	left := 2*index + 1
+	// 如果有左孩子，有没有右孩子，可能有可能没有！
+	for left < h.size {
+		// 把较大孩子的下标，给largest
+		largest := left
+		// 如果有右孩子，且右孩子的值大于左孩子的值
+		if left+1 < h.size && h.heap[left+1] > h.heap[left] {
+			largest = left + 1
+		}
+		if h.heap[largest] <= h.heap[index] {
+			break
+		}
+		// index和较大孩子，要互换
+		h.swap(largest, index)
+		// index更新为较大孩子的下标
+		index = largest
+		left = 2*index + 1
+	}
+}
+
+func (h *MyMaxHeap) swap(i, j int) {
+	h.heap[i], h.heap[j] = h.heap[j], h.heap[i]
+}
+
+func main() {
+	// ... Code for testing MyMaxHeap as in Java main method
+	// You can use the same logic for testing as in the Java code.
+	// Here, we'll just demonstrate basic operations.
+	rand.Seed(time.Now().UnixNano())
+
+	mh := NewMyMaxHeap(10)
+	for i := 0; i < 5; i++ {
+		value := rand.Intn(100)
+		fmt.Printf("Pushing %d\n", value)
+		mh.Push(value)
+	}
+
+	for !mh.IsEmpty() {
+		value, err := mh.Pop()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Popped %d\n", value)
+		}
+	}
 }
--- algo/zuo/ti_xi_study/class06/Code04_SortArrayDistanceLessK/Code04_SortArrayDistanceLessK.go
+++ algo/zuo/ti_xi_study/class06/Code04_SortArrayDistanceLessK/Code04_SortArrayDistanceLessK.go
@@ -1,5 +1,150 @@
 package main
 
-func main() {
+import (
+	"container/heap"
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
+// 排序数组，数组的任何一个数移动的距离不会超过k
+func sortedArrDistanceLessK(arr []int, k int) {
+	if k == 0 {
+		return
+	}
+
+	// 创建一个优先队列（小根堆）
+	minHeap := &IntHeap{}
+	heap.Init(minHeap)
+	index := 0
+
+	// 首先将前k个元素放入堆中
+	for ; index <= min(len(arr)-1, k-1); index++ {
+		heap.Push(minHeap, arr[index])
+	}
+
+	// 遍历数组剩余的元素，并同时从堆中取出元素放回数组
+	i := 0
+	for ; index < len(arr); i, index = i+1, index+1 {
+		heap.Push(minHeap, arr[index])
+		arr[i] = heap.Pop(minHeap).(int)
+	}
+
+	// 将堆中剩余的元素取出放回数组
+	for minHeap.Len() > 0 {
+		arr[i] = heap.Pop(minHeap).(int)
+		i++
+	}
+}
+
+// IntHeap 是一个实现了heap.Interface的int类型的最小堆
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// min 返回两个整数中较小的一个
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// randomArrayNoMoveMoreK 生成一个随机数组，数组中任意数的移动距离不超过k
+func randomArrayNoMoveMoreK(maxSize, maxValue, k int) []int {
+	arr := make([]int, rand.Intn(maxSize+1))
+	for i := range arr {
+		arr[i] = rand.Intn(maxValue+1) - rand.Intn(maxValue)
+	}
+
+	// 先排序
+	sort.Ints(arr)
+
+	// 随机交换，保证每个数的移动距离不超过k
+	isSwap := make([]bool, len(arr))
+	for i := range arr {
+		j := min(i+rand.Intn(k+1), len(arr)-1)
+		if !isSwap[i] && !isSwap[j] {
+			isSwap[i] = true
+			isSwap[j] = true
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	return arr
+}
+
+// copyArray 复制一个整型数组
+func copyArray(arr []int) []int {
+	res := make([]int, len(arr))
+	copy(res, arr)
+	return res
+}
+
+// isEqual 判断两个数组是否相等
+func isEqual(arr1, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	for i := range arr1 {
+		if arr1[i] != arr2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// printArray 打印一个整型数组
+func printArray(arr []int) {
+	for _, v := range arr {
+		fmt.Print(v, " ")
+	}
+	fmt.Println()
+}
+
+func main() {
+	fmt.Println("test begin")
+	rand.Seed(time.Now().UnixNano())
+	testTime := 500000
+	maxSize := 100
+	maxValue := 100
+	succeed := true
+
+	for i := 0; i < testTime; i++ {
+		k := rand.Intn(maxSize) + 1
+		arr := randomArrayNoMoveMoreK(maxSize, maxValue, k)
+		arr1 := copyArray(arr)
+		arr2 := copyArray(arr)
+
+		sortedArrDistanceLessK(arr1, k)
+		sort.Ints(arr2) // Go的sort.Ints实现了完整的排序
+
+		if !isEqual(arr1, arr2) {
+			succeed = false
+			fmt.Println("K:", k)
+			printArray(arr)
+			printArray(arr1)
+			printArray(arr2)
+			break
+		}
+	}
+	if succeed {
+		fmt.Println("Nice!")
+	} else {
+		fmt.Println("Fucking fucked!")
+	}
 }

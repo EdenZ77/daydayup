97c8078ba4ea65fd82e840ff0ec0a9c34b2aeeb0
zhuqiqi <zhuqiqi@skyguard.com.cn>
2025-07-14 18:22:00 +0800

    err

--- base/err/1/test1.go
+++ base/err/1/test1.go
@@ -10,7 +10,7 @@ var ErrSentinel = errors.New("the underlying sentinel error")
 func main() {
 	err1 := fmt.Errorf("wrap sentinel: %w", ErrSentinel)
 	err2 := fmt.Errorf("wrap err1: %w", err1)
-	println(err2 == ErrSentinel) //false
+	//println(err2 == ErrSentinel) //false
 	if errors.Is(err2, ErrSentinel) {
 		println("err2 is ErrSentinel")
 		return
--- base/err/3/test3.go
+++ base/err/3/test3.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+type MyError struct {
+	Code    int
+	Message string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("error %d: %s", e.Code, e.Message)
+}
+
+func main() {
+	err := &MyError{Code: 404, Message: "Not Found"}
+
+	var target *MyError
+	if errors.As(err, &target) {
+		fmt.Printf("匹配成功: Code=%d, Message=%s\n", target.Code, target.Message)
+	} else {
+		fmt.Println("未匹配到MyError类型")
+	}
 }
